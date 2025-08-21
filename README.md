0f261324b21b6ed9a3ff0a1658583e70af606ef5
valentin <valentinfltv@gmail.com>
2025-08-21 16:53:58 +0300

    Refactoring after review: added package.json, improved README, made project more readable

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# js_test_repository
-Here will be stored executed tasks for js, node.js etc.
+# Timers Manager
+
+A Node.js utility for managing timers with pause, resume, and remove functionality.
+
+# Quickstart and Installation
+
+1. Clone the repository using git:
+   git clone https://github.com/valentinfltv/timers-manager.git
+   cd timers-manager
+
+2. Start app with:
+   npm start
+   
+# Expected output
+
+Default text: 
+   Started
+   t2 result: 3
+   t1 fired
+   
+Yet it will vary depending on the inputs of index.js file   
--- src/timersManager.js
+++ src/timersManager.js
@@ -1,7 +1,7 @@
 class TimersManager {
  constructor() {
- this.timers = [];
- this.started = false;
+   this.timers = [];
+   this.started = false;
  }
 
  _validateTimer(timer) {
@@ -125,29 +125,4 @@ class TimersManager {
   }
 
 }
-
-
-const manager = new TimersManager();
-
-const t1 = {
-  name: 't1',
-  delay: 1000,
-  interval: false,
-  job: () => { console.log('t1 fired'); }
-};
-
-const t2 = {
-  name: 't2',
-  delay: 2000,
-  interval: false,
-  job: (a, b) => console.log(`t2 result: ${a + b}`)
-};
-
-
-
-manager.add(t1).add(t2, 1, 2);
-manager.start();
-console.log("Started");
-
-setTimeout(() => manager.pause('t1'), 500);
-setTimeout(() => manager.resume('t1'), 1500);
+module.exports = TimersManager; 
